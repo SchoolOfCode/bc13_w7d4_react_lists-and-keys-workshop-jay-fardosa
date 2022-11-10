b48f79d463df45069630eb608bf1508ae0283f0d
jayjay1878 <jaybartley777@outlook.com>
2022-11-10 19:31:26 +0000

    workshop
    task 6

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -9,7 +9,13 @@ import Post from '../Post';
 function Main() {
   const [posts, setPosts] = useState(samplePosts);
 
-  return <main id="main"></main>;
+  return <main id="main">
+    {posts.map(({text, postId, title, date, author, highlights, image}) => {
+     return <Post body={text} key={postId} title={title} date={date} author={author} highlights={highlights} image={image}></Post>
+    })}
+  </main>;
 }
 
 export default Main;
+
+
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -2,8 +2,19 @@ import React from 'react';
 
 import './index.css';
 
-function Post() {
-  return <article></article>;
+function Post({body, title, date, author, highlights, image}) {
+  return <article>
+    <h1>{title}</h1>
+    <p>{author}</p>
+    <p>{date}</p>
+    <p>{body}</p>
+    {highlights.map((highlight, index) => {
+      return <li key={index}>{highlight}</li>
+    })}
+    <img src = {image.link} alt={image.alt}></img>
+  </article>
 }
 
 export default Post;
+
+
